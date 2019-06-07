423839f9601191dbf076ef4840ec3e57bd396ce0
GrayHat12 <grayhathacks10@gmail.com>
2019-06-07 23:34:57 +0530

    minor updates

--- app/src/main/java/ghh/grayhat/graybot_sr/MainActivity.java
+++ app/src/main/java/ghh/grayhat/graybot_sr/MainActivity.java
@@ -4,6 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Handler;
 import android.os.PowerManager;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +14,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.chaquo.python.PyObject;
 import com.chaquo.python.Python;
@@ -25,14 +29,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -48,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
     PyObject pafy;
     int currentSong;
     PyObject ndat;
+    SeekBar seek;
+    private Handler mHandler;
+    ProgressBar pg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,30 +77,60 @@ public class MainActivity extends AppCompatActivity {
             next.setText(">>");
             fff=(Button)findViewById(R.id.fff);
             fff.setText(">>>");
-
+            pg=(ProgressBar)findViewById(R.id.progress);
             try
             {
                 if (! Python.isStarted()) {
                     Python.start(new AndroidPlatform(getBaseContext()));
                 }
                 pafy = Python.getInstance().getModule("pafy");
-                mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
             }
             catch(Exception e)
             {
                 e.printStackTrace();
             }
+            seek=(SeekBar)findViewById(R.id.seek);
+            pg.setVisibility(pg.INVISIBLE);
+            seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    seekAudio(progress,fromUser);
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+
+                }
+            });
+
         }
     }
 
-    public static List getLinks(String query)
+    private void seekAudio(int progress, boolean fromUser) {
+        if(mediaPlayer != null && fromUser){
+            mediaPlayer.seekTo(progress);
+        }
+        else if(fromUser)
+        {
+            Toast.makeText(this,"No Audio Playing",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public List getLinks(String query)
     {
         List<String> links=new ArrayList<>();
         try {
+            pg.setVisibility(pg.VISIBLE);
             URL url = new URL("https://www.youtube.com/results?search_query=" + URLEncoder.encode(query, "UTF-8"));
             HttpsURLConnection urlConnection=(HttpsURLConnection) url.openConnection();
             urlConnection.setRequestProperty("User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
             String code = toString(urlConnection.getInputStream());
+            pg.setVisibility(pg.INVISIBLE);
             while(code.contains("watch?v="))
             {
                 code=code.substring(code.indexOf("watch?v="));
@@ -138,26 +170,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void search(View view) {
         String query=""+queryS.getText();
+        Toast.makeText(this,"SEARCHING...",Toast.LENGTH_SHORT).show();
         songs = getLinks(query);
         isSongStarted=false;
         currentSong=0;
         setView();
+        Toast.makeText(this,"SEARCHING DONE",Toast.LENGTH_SHORT).show();
     }
 
     private void setView() {
+        pg.setVisibility(pg.VISIBLE);
         ndat=pafy.callAttr("new",""+songs.get(currentSong));
         PyObject titl=ndat.get("title");
         PyObject img=ndat.get("thumb");
+        pg.setVisibility(pg.INVISIBLE);
         try {
             thumb.setImageBitmap(getBitmapFromURL(""+img));
+            title.setText(""+titl);
+            PyObject chnl=ndat.get("author");
+            channel.setText(""+chnl);
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
-        title.setText(""+titl);
-        PyObject chnl=ndat.get("author");
-        channel.setText(""+chnl);
     }
 
     public static Bitmap getBitmapFromURL(String src) {
@@ -179,13 +215,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String getUrl() {
-        PyObject ndat=pafy.callAttr("new",""+songs.get(currentSong));
         PyObject url=ndat.callAttr("getbestaudio").get("url");
         return ""+url;
     }
 
+    private int getDuration()
+    {
+        PyObject lngth=ndat.get("length");
+        return Integer.parseInt(""+lngth);
+    }
+
     private void playAudio() {
         System.out.println("SONG NUMBER : "+currentSong);
+        Toast.makeText(this,"PLAYING : "+ndat.get("title"),Toast.LENGTH_LONG).show();
         if(currentSong>=songs.size())
         {
             return;
@@ -194,17 +236,37 @@ public class MainActivity extends AppCompatActivity {
         isSongStarted=true;
         setView();
         try {
+            mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
             mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
             mediaPlayer.setDataSource(getUrl());
+            pg.setVisibility(pg.VISIBLE);
             mediaPlayer.prepare();
+            pg.setVisibility(pg.INVISIBLE);
             mediaPlayer.start();
+            seek.setMax(getDuration()*1000);
             mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                 @Override
                 public void onCompletion(MediaPlayer mp) {
-                    setView();
+                    play.setText("|>");
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                    isSongStarted=false;
+                    mediaPlayer=null;
                 }
             });
             isSongStarted=true;
+            mHandler=new Handler();
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(mediaPlayer!=null)
+                    {
+                        int mCurrentPosition=mediaPlayer.getCurrentPosition();
+                        seek.setProgress(mCurrentPosition);
+                    }
+                    mHandler.postDelayed(this,1);
+                }
+            });
         }
         catch (Exception er)
         {
@@ -240,6 +302,7 @@ public class MainActivity extends AppCompatActivity {
     public void play(View view) {
         if(!isSongStarted)
         {
+            Toast.makeText(this,"Loading",Toast.LENGTH_LONG).show();
             play.setText("O");
             playAudio();
             play.setText("||");
@@ -252,7 +315,6 @@ public class MainActivity extends AppCompatActivity {
         }
         else
         {
-            play.setText("O");
             mediaPlayer.start();
             play.setText("||");
         }
@@ -260,10 +322,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void runNext(View view) {
         currentSong+=1;
+        Toast.makeText(this,"Loading",Toast.LENGTH_LONG).show();
         try {
-            mediaPlayer.stop();
-            mediaPlayer.release();
-            mediaPlayer=null;
+            if(mediaPlayer!=null) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                mediaPlayer = null;
+            }
             playAudio();
         }
         catch (Exception e)
@@ -275,4 +340,5 @@ public class MainActivity extends AppCompatActivity {
     public void runfff(View view) {
         mediaPlayer.seekTo(mediaPlayer.getCurrentPosition()+5000);
     }
+
 }
